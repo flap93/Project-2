ffc7bed2d383e2b4c2c6e8386d6b203eb8623b83
Ana Ortez <anaortez23@gmail.com>
2021-02-07 20:43:05 -0500

    delete route complete

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
-  // const scheduler = require('./configs/scheduler.config');
-  // const checkAndSendNecessaryMessages = require('./configs/marcos.config');
 const app = express();
 
 
@@ -18,25 +16,16 @@ const app = express();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
-// const schedule = require('node-schedule-tz');
+
 
 // Set up the database
 require('./configs/db.config');
 require('./configs/session.config')(app);
-// require('./configs/api.config');
-// require('./configs/messagesWorker');
-// require('./configs/scheduler.config');
-// require('./configs/marcos.config');
 
 
 const bindUserToLocals = require("./configs/user-locals.config");
 app.use(bindUserToLocals);
 
-// Routers
-const indexRouter = require('./routes/index.routes');
-const authRouter = require('./routes/auth.routes');
-const userInterRouter = require('./routes/userInter.routes');
-const reminderRouter = require('./routes/reminder.routes');
 
 // Express View engine setup
 
@@ -54,17 +43,24 @@ app.use(cookieParser());
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-// Routes middleware
-app.use('/', indexRouter);
-app.use('/', authRouter);
-app.use('/', userInterRouter);
-app.use('/', reminderRouter);
+// default value for title local
+app.locals.title = "Remind Me";
+app.locals.slogan = "Remembering is the key";
+
+
+
+// Routes 
+app.use('/', require('./routes/index.routes'));
+app.use('/', require('./routes/auth.routes'));
+app.use('/',  require('./routes/reminder.routes'));
+
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => next(createError(404)));
 
 // Catch all error handler
 app.use((error, req, res) => {
+
   // Set error information, with stack only available in development
   res.locals.message = error.message;
   res.locals.error = req.app.get('env') === 'development' ? error : {};
@@ -74,7 +70,7 @@ app.use((error, req, res) => {
   res.render('error');
 
 });
-//scheduler.start();
+
 
 
 module.exports = app;
